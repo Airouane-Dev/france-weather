dbadc5f6ca49f7a380abb81044eb9490e817498e
Airouane <erwan.cadobts@gmail.com>
2021-02-21 21:47:05 +0100

    FEAT: ADD scss and navbar

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
     <div id="nav">
-      <router-link to="/">Dashboard</router-link> |
-      <router-link to="/carte">Carte</router-link> |
-      <router-link to="/statistique">Statistique</router-link> |
-      <router-link to="/compte">Compte</router-link> |
+      <span id="title">
+          France Weather
+        </span>
+      <ul>
+        <li>
+          <router-link to="/">Dashboard</router-link>
+        </li>
+        <li>
+          <router-link to="/carte">Carte</router-link>
+        </li>
+        <li>
+          <router-link to="/statistique">Statistique</router-link>
+        </li>
+        <li>
+          <router-link to="/compte">Compte</router-link>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+npm install -D css-loader
+<style lang="scss" scoped>
+  @import './stylesheet/main.scss';
 </style>
