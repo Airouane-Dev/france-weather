f06247937f506d37e139ff750ca3867cffd8a4e0
marth154 <37938187+marth154@users.noreply.github.com>
2021-02-23 18:18:15 +0100

    Conifig firebase / debut Card

--- src/components/dashboard/CardOverView.vue
+++ src/components/dashboard/CardOverView.vue
@@ -1,31 +1,20 @@
 <template>
     <div>
-        <div class="flex flex-wrap overflow-hidden">
-
-            <div class="w-1/2 overflow-hidden">
-                {{ ville }}
+        <div class="grid-container">
+            <div class="ville">
+                <div class="nomVille">{{ ville }}</div>
+                <div class="jours">Samedi 20/02/2021</div>
+                <div class="heure">22:54</div>
             </div>
-
-            <div class="w-1/2 overflow-hidden">
-                <fa :icon="iconMeteo"/>
+            <div class="icon"><fa :icon="iconMeteo"/></div>
+            <div class="temperature">
+                <div class="titreTemperature">Température</div>
+                <div class="valeurTemperature">{{temperature}}°c</div>
             </div>
-
-            <div class="w-1/2 overflow-hidden">
-                Température
+            <div class="humidite">
+                <div class="titreHumidite">Humidité</div>
+                <div class="valeurHumidite">{{humidite}}%</div>
             </div>
-
-            <div class="w-1/2 overflow-hidden">
-                Humidité
-            </div>
-
-            <div class="w-1/2 overflow-hidden">
-                {{temperature}}°c
-            </div>
-
-            <div class="w-1/2 overflow-hidden">
-                {{humidite}}%
-            </div>
-
         </div>
     </div>
 </template>
@@ -40,6 +29,103 @@ export default {
     ]
 }
 </script>
-<style lang="">
-    
+<style lang="scss">
+.grid-container {
+  background-color: rgba(184, 255, 242, 0.43);
+  width: 90%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 40px;
+  border-radius: 10px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  gap: 0px 0px;
+  grid-template-areas:
+    "ville icon"
+    "temperature humidite";
+}
+
+.ville {
+    margin-left: 10%;
+    margin-top: 10%;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr 1fr 1fr;
+    gap: 0px 0px;
+    grid-template-areas:
+        "nomVille"
+        "jour"
+        "heure";
+    grid-area: ville;
+}
+
+.nomVille { 
+    grid-area: nomVille; 
+    font-size: 25px;
+    font-weight: 600;
+}
+
+.jour { 
+    grid-area: jour;
+    font-size: 12px; 
+}
+
+.heure { 
+    grid-area: heure;
+    font-size: 12px;  
+}
+
+.icon { 
+    margin-top: 10%;
+    grid-area: icon;
+}
+
+.temperature {
+    margin-left: 10%;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr 1fr;
+    gap: 0px 0px;
+    grid-template-areas:
+        "titreTemperature titreTemperature"
+        "valeurTemperature valeurTemperature";
+    grid-area: temperature;
+}
+
+.titreTemperature { 
+    grid-area: titreTemperature; 
+    font-size: 14px;
+    font-weight: 500;
+}
+
+.valeurTemperature { 
+    grid-area: valeurTemperature; 
+    font-size: 25px;
+    font-weight: 600;
+}
+
+.humidite {
+    margin-left: 10%;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr;
+    gap: 0px 0px;
+    grid-template-areas:
+       "titreHumidite titreHumidite titreHumidite"
+       "valeurHumidite valeurHumidite valeurHumidite";
+    grid-area: humidite;
+}
+
+.titreHumidite { 
+    grid-area: titreHumidite; 
+    font-size: 14px;
+    font-weight: 500;
+}
+
+.valeurHumidite { 
+    grid-area: valeurHumidite;
+    font-size: 25px;
+    font-weight: 600; 
+}
 </style>
